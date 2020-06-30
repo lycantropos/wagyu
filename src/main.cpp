fd7d6442845f4d773dddfaa6bb5592329abb8c0c
Azat Ibrakov <azatibrakov@gmail.com>
2020-07-01 02:51:22 +0700

    Complete bound 'Point.__repr__' magic method

--- src/main.cpp
+++ src/main.cpp
@@ -15,20 +15,24 @@ namespace py = pybind11;
 using coordinate_t = double;
 using Point = mapbox::geometry::point<coordinate_t>;
 
-static std::ostringstream make_stream() {
-  std::ostringstream stream;
-  stream.precision(std::numeric_limits<coordinate_t>::digits10 + 2);
-  return stream;
-}
+static std::string bool_repr(bool value) { return py::str(py::bool_(value)); }
 
-static std::string point_repr(const Point& self) {
-  auto stream = make_stream();
-  stream << C_STR(MODULE_NAME) "." POINT_NAME "(" << self.x << ", " << self.y
-         << ")";
+template <class Object>
+std::string repr(const Object& object) {
+  std::ostringstream stream;
+  stream.precision(std::numeric_limits<double>::digits10 + 2);
+  stream << object;
   return stream.str();
 }
 
-static std::string bool_repr(bool value) { return py::str(py::bool_(value)); }
+namespace mapbox {
+namespace geometry {
+static std::ostream& operator<<(std::ostream& stream, const Point& point) {
+  return stream << C_STR(MODULE_NAME) "." POINT_NAME "(" << point.x << ", "
+                << point.y << ")";
+}
+}  // namespace geometry
+}  // namespace mapbox
 
 PYBIND11_MODULE(MODULE_NAME, m) {
   m.doc() = R"pbdoc(
@@ -47,7 +51,7 @@ PYBIND11_MODULE(MODULE_NAME, m) {
                          tuple[1].cast<coordinate_t>());
           }))
       .def(py::self == py::self)
-      .def("__repr__", point_repr)
+      .def("__repr__", repr<Point>)
       .def_readonly("x", &Point::x)
       .def_readonly("y", &Point::y);
 
