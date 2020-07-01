a014a82f66942495c4d5fcce467348fda431916f
Azat Ibrakov <azatibrakov@gmail.com>
2020-07-02 02:22:07 +0700

    Add test for bound 'Ring.set_stats' method

--- tests/bind_tests/ring_tests/strategies.py
+++ tests/bind_tests/ring_tests/strategies.py
@@ -1,4 +1,5 @@
-from _wagyu import (Point,
+from _wagyu import (Box,
+                    Point,
                     PointNode,
                     Ring)
 from hypothesis import strategies
@@ -9,6 +10,7 @@ from tests.utils import to_maybe
 booleans = strategies.booleans()
 sizes = strategies.integers(0, 65535)
 points = strategies.builds(Point, floats, floats)
+boxes = strategies.builds(Box, points, points)
 maybe_points_nodes = to_maybe(strategies.builds(PointNode, floats, floats))
 maybe_rings = to_maybe(strategies.deferred(lambda: rings))
 maybe_rings_lists = strategies.lists(maybe_rings)
---
+++ tests/bind_tests/ring_tests/test_set_stats.py
@@ -0,0 +1,15 @@
+from _wagyu import (Box,
+                    Ring)
+from hypothesis import given
+
+from . import strategies
+
+
+@given(strategies.rings, strategies.floats, strategies.sizes, strategies.boxes)
+def test_basic(ring: Ring, area: float, size: int, box: Box) -> None:
+    result = ring.set_stats(area, size, box)
+
+    assert result is None
+    assert ring.area == area
+    assert ring.size == size
+    assert ring.box == box
