33748221f5b7becda121d01d0b258a4e14aa9d82
Azat Ibrakov <azatibrakov@gmail.com>
2020-07-03 19:42:46 +0700

    Add test for bound 'Polygon' class pickling

---
+++ tests/bind_tests/polygon_tests/test_pickle.py
@@ -0,0 +1,10 @@
+from _wagyu import Polygon
+from hypothesis import given
+
+from tests.utils import pickle_round_trip
+from . import strategies
+
+
+@given(strategies.polygons)
+def test_round_trip(polygon: Polygon) -> None:
+    assert pickle_round_trip(polygon) == polygon
