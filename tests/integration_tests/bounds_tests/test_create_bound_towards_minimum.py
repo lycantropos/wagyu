74fab6beb7b803486811dbd0329bf198e47fbce5
Azat Ibrakov <azatibrakov@gmail.com>
2020-07-08 01:53:32 +0700

    Add test for bound & ported 'create_bound_towards_minimum' functions

---
+++ tests/integration_tests/bounds_tests/test_create_bound_towards_minimum.py
@@ -0,0 +1,17 @@
+import copy
+
+from _wagyu import create_bound_towards_minimum as bound
+from hypothesis import given
+
+from tests.utils import (BoundPortedEdgesListsPair,
+                         are_bound_ported_bounds_equal)
+from wagyu.bound import create_bound_towards_minimum as ported
+from . import strategies
+
+
+@given(strategies.edges_lists_pairs)
+def test_basic(edges_lists_pair: BoundPortedEdgesListsPair):
+    bound_edges, ported_edges = edges_lists_pair
+
+    assert are_bound_ported_bounds_equal(bound(bound_edges),
+                                         ported(copy.deepcopy(ported_edges)))
