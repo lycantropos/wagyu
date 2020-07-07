e403fe2e646c9c7091945cded6b8d2ea5bcb110f
Azat Ibrakov <azatibrakov@gmail.com>
2020-07-07 19:11:34 +0700

    Complete type annotations for 'LocalMinimumList.__eq__' magic method test

--- tests/binding_tests/local_minimum_list_tests/test_equals.py
+++ tests/binding_tests/local_minimum_list_tests/test_equals.py
@@ -1,4 +1,4 @@
-from _wagyu import LinearRing
+from _wagyu import LocalMinimumList
 from hypothesis import given
 
 from tests.utils import (equivalence,
@@ -7,23 +7,23 @@ from . import strategies
 
 
 @given(strategies.local_minimum_lists)
-def test_reflexivity(local_minimum_list: LinearRing) -> None:
+def test_reflexivity(local_minimum_list: LocalMinimumList) -> None:
     assert local_minimum_list == local_minimum_list
 
 
 @given(strategies.local_minimum_lists,
        strategies.local_minimum_lists)
-def test_symmetry(first_local_minimum_list: LinearRing,
-                  second_local_minimum_list: LinearRing) -> None:
+def test_symmetry(first_local_minimum_list: LocalMinimumList,
+                  second_local_minimum_list: LocalMinimumList) -> None:
     assert equivalence(first_local_minimum_list == second_local_minimum_list,
                        second_local_minimum_list == first_local_minimum_list)
 
 
 @given(strategies.local_minimum_lists, strategies.local_minimum_lists,
        strategies.local_minimum_lists)
-def test_transitivity(first_local_minimum_list: LinearRing,
-                      second_local_minimum_list: LinearRing,
-                      third_local_minimum_list: LinearRing) -> None:
+def test_transitivity(first_local_minimum_list: LocalMinimumList,
+                      second_local_minimum_list: LocalMinimumList,
+                      third_local_minimum_list: LocalMinimumList) -> None:
     assert implication(
             first_local_minimum_list == second_local_minimum_list
             and second_local_minimum_list == third_local_minimum_list,
@@ -31,8 +31,8 @@ def test_transitivity(first_local_minimum_list: LinearRing,
 
 
 @given(strategies.local_minimum_lists, strategies.local_minimum_lists)
-def test_connection_with_inequality(first_local_minimum_list: LinearRing,
-                                    second_local_minimum_list: LinearRing
+def test_connection_with_inequality(first_local_minimum_list: LocalMinimumList,
+                                    second_local_minimum_list: LocalMinimumList
                                     ) -> None:
     assert equivalence(
             not first_local_minimum_list == second_local_minimum_list,
