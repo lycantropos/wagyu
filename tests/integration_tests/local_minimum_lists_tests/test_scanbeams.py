061214203b6b4d37196275dfca446ed88c496baa
Azat Ibrakov <azatibrakov@gmail.com>
2020-07-08 16:05:30 +0700

    Add test for bound & ported 'LocalMinimumList.scanbeams' properties

---
+++ tests/integration_tests/local_minimum_lists_tests/test_scanbeams.py
@@ -0,0 +1,11 @@
+from hypothesis import given
+
+from tests.utils import BoundPortedLocalMinimumListsPair
+from . import strategies
+
+
+@given(strategies.local_minimum_lists_pairs)
+def test_basic(pair: BoundPortedLocalMinimumListsPair) -> None:
+    bound, ported = pair
+
+    assert bound.scanbeams == ported.scanbeams
