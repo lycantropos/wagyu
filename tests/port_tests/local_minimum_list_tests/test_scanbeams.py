1ee29366e4a3baf77e3ccf27d8c96b86ec157f83
Azat Ibrakov <azatibrakov@gmail.com>
2020-07-08 16:05:19 +0700

    Add test for ported 'LocalMinimumList.scanbeams' property

---
+++ tests/port_tests/local_minimum_list_tests/test_scanbeams.py
@@ -0,0 +1,16 @@
+from hypothesis import given
+
+from wagyu.hints import Coordinate
+from wagyu.local_minimum import LocalMinimumList
+from . import strategies
+
+
+@given(strategies.local_minimum_lists)
+def test_basic(local_minimum_list: LocalMinimumList) -> None:
+    assert all(isinstance(element, Coordinate)
+               for element in local_minimum_list.scanbeams)
+
+
+@given(strategies.local_minimum_lists)
+def test_properties(local_minimum_list: LocalMinimumList) -> None:
+    assert len(local_minimum_list.scanbeams) == len(local_minimum_list)
