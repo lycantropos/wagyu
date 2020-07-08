65480befa749d53d0781160319228ee9d16d9291
Azat Ibrakov <azatibrakov@gmail.com>
2020-07-08 14:28:45 +0700

    Complete test for bound 'RingManager' class initialization

--- tests/binding_tests/ring_manager_tests/strategies.py
+++ tests/binding_tests/ring_manager_tests/strategies.py
@@ -1,4 +1,26 @@
-from _wagyu import RingManager
+from _wagyu import (Point,
+                    PointNode,
+                    Ring,
+                    RingManager)
 from hypothesis import strategies
 
-ring_managers = strategies.builds(RingManager)
+from tests.strategies import coordinates
+from tests.utils import to_maybe
+
+booleans = strategies.booleans()
+sizes = strategies.integers(0, 65535)
+points = strategies.builds(Point, coordinates, coordinates)
+points_nodes = strategies.builds(PointNode, coordinates, coordinates)
+maybe_points_nodes = to_maybe(points_nodes)
+maybe_rings = to_maybe(strategies.deferred(lambda: rings))
+maybe_rings_lists = strategies.lists(maybe_rings)
+rings = strategies.builds(Ring, sizes, maybe_rings_lists, maybe_points_nodes,
+                          maybe_points_nodes, booleans)
+points_lists = strategies.lists(points)
+points_nodes_lists = strategies.lists(points_nodes)
+maybe_points_nodes_lists = strategies.lists(maybe_points_nodes)
+rings_lists = strategies.lists(rings)
+ring_managers = strategies.builds(RingManager, maybe_rings_lists,
+                                  maybe_points_nodes_lists, points_lists,
+                                  points_nodes_lists, rings_lists,
+                                  points_nodes_lists, sizes)
--- tests/binding_tests/ring_manager_tests/test_initialization.py
+++ tests/binding_tests/ring_manager_tests/test_initialization.py
@@ -1,13 +1,32 @@
-from _wagyu import RingManager
+from typing import (List,
+                    Optional)
+
+from _wagyu import (Point,
+                    PointNode,
+                    Ring,
+                    RingManager)
+from hypothesis import given
+
+from . import strategies
 
 
-def test_basic() -> None:
-    result = RingManager()
+@given(strategies.maybe_rings_lists, strategies.maybe_points_nodes_lists,
+       strategies.points_lists, strategies.points_nodes_lists,
+       strategies.rings_lists, strategies.points_nodes_lists, strategies.sizes)
+def test_basic(children: List[Optional[Ring]],
+               all_nodes: List[Optional[PointNode]],
+               hot_pixels: List[Point],
+               nodes: List[PointNode],
+               rings: List[Ring],
+               storage: List[PointNode],
+               index: int) -> None:
+    result = RingManager(children, all_nodes, hot_pixels, nodes, rings,
+                         storage, index)
 
-    assert not result.index
-    assert not result.all_nodes
-    assert not result.nodes
-    assert not result.storage
-    assert not result.hot_pixels
-    assert not result.rings
-    assert not result.children
+    assert result.children == children
+    assert result.all_nodes == all_nodes
+    assert result.hot_pixels == hot_pixels
+    assert result.nodes == nodes
+    assert result.storage == storage
+    assert result.rings == rings
+    assert result.index == index
