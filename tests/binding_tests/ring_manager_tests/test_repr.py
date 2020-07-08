7e158251e5c8284094763d5e1f1a36afd850c0cf
Azat Ibrakov <azatibrakov@gmail.com>
2020-07-08 14:37:55 +0700

    Complete test for bound 'RingManager.__repr__' magic method

--- tests/binding_tests/ring_manager_tests/test_repr.py
+++ tests/binding_tests/ring_manager_tests/test_repr.py
@@ -1,3 +1,5 @@
+import sys
+
 from _wagyu import RingManager
 from hypothesis import given
 
@@ -10,3 +12,10 @@ def test_basic(ring_manager: RingManager) -> None:
 
     assert result.startswith(RingManager.__module__)
     assert RingManager.__qualname__ in result
+
+
+@given(strategies.ring_managers)
+def test_round_trip(ring_manager: RingManager) -> None:
+    result = repr(ring_manager)
+
+    assert eval(result, sys.modules) == ring_manager
