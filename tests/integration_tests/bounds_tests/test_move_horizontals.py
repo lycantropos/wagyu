8b09679dc0dd0a0897bc6fde355cf23bc2dbcb72
Azat Ibrakov <azatibrakov@gmail.com>
2020-07-09 20:50:09 +0700

    Add test for bound & ported 'Bound.move_horizontals' methods

---
+++ tests/integration_tests/bounds_tests/test_move_horizontals.py
@@ -0,0 +1,18 @@
+from hypothesis import given
+
+from tests.utils import (BoundPortedBoundsPair,
+                         are_bound_ported_bounds_equal)
+from . import strategies
+
+
+@given(strategies.bounds_pairs, strategies.bounds_pairs)
+def test_basic(first_pair: BoundPortedBoundsPair,
+               second_pair: BoundPortedBoundsPair) -> None:
+    first_bound, first_ported = first_pair
+    second_bound, second_ported = second_pair
+
+    first_bound.move_horizontals(second_bound)
+    first_ported.move_horizontals(second_ported)
+
+    assert are_bound_ported_bounds_equal(first_bound, first_ported)
+    assert are_bound_ported_bounds_equal(second_bound, second_ported)
