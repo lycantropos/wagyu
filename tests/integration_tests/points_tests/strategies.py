4c98097772a554205f8d301599728017d7435d87
Azat Ibrakov <azatibrakov@gmail.com>
2020-07-10 02:08:23 +0700

    Add test for bound & ported 'Point.round' methods

--- tests/integration_tests/points_tests/strategies.py
+++ tests/integration_tests/points_tests/strategies.py
@@ -1,8 +1,11 @@
 from hypothesis import strategies
 
-from tests.strategies import coordinates
+from tests.strategies import (coordinates,
+                              floats)
 from tests.utils import to_bound_with_ported_points_pair
 
 coordinates = coordinates
 points_pairs = strategies.builds(to_bound_with_ported_points_pair,
                                  coordinates, coordinates)
+float_points_pairs = strategies.builds(to_bound_with_ported_points_pair,
+                                       floats, floats)
---
+++ tests/integration_tests/points_tests/test_round.py
@@ -0,0 +1,12 @@
+from hypothesis import given
+
+from tests.utils import (BoundPortedPointsPair,
+                         are_bound_ported_points_equal)
+from . import strategies
+
+
+@given(strategies.float_points_pairs)
+def test_basic(points_pair: BoundPortedPointsPair) -> None:
+    bound, ported = points_pair
+
+    assert are_bound_ported_points_equal(bound.round(), ported.round())
