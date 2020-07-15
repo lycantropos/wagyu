ae96f4dcebef344a1c39fefca3f7cc11af90c5bf
Azat Ibrakov <azatibrakov@gmail.com>
2020-07-15 13:10:29 +0700

    Complete test for bound 'Bound.__repr__' magic method

--- tests/binding_tests/bound_tests/test_repr.py
+++ tests/binding_tests/bound_tests/test_repr.py
@@ -1,3 +1,5 @@
+import sys
+
 from _wagyu import Bound
 from hypothesis import given
 
@@ -10,3 +12,10 @@ def test_basic(bound: Bound) -> None:
 
     assert result.startswith(Bound.__module__)
     assert Bound.__qualname__ in result
+
+
+@given(strategies.bounds)
+def test_round_trip(bound: Bound) -> None:
+    result = repr(bound)
+
+    assert eval(result, sys.modules) == bound
