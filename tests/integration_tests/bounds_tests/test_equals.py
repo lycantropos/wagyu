adaa1150cae879c13220fc8335599c8aa15b532a
Azat Ibrakov <azatibrakov@gmail.com>
2020-07-15 13:37:02 +0700

    Add test for bound & ported 'Bound.__eq__' magic methods

---
+++ tests/integration_tests/bounds_tests/test_equals.py
@@ -0,0 +1,15 @@
+from hypothesis import given
+
+from tests.utils import (BoundPortedBoundsPair,
+                         equivalence)
+from . import strategies
+
+
+@given(strategies.bounds_pairs, strategies.bounds_pairs)
+def test_basic(first_pair: BoundPortedBoundsPair,
+               second_pair: BoundPortedBoundsPair) -> None:
+    first_bound, first_ported = first_pair
+    second_bound, second_ported = second_pair
+
+    assert equivalence(first_bound == second_bound,
+                       first_ported == second_ported)
