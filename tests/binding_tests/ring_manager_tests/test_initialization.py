a3d47fec921e9b6ca09e1877ad8a7cd9d52eef72
Azat Ibrakov <azatibrakov@gmail.com>
2020-07-18 23:34:55 +0700

    Complete test for bound 'RingManager' class initialization

--- tests/binding_tests/ring_manager_tests/test_initialization.py
+++ tests/binding_tests/ring_manager_tests/test_initialization.py
@@ -13,15 +13,15 @@ from . import strategies
        strategies.rings_lists, strategies.sizes)
 def test_basic(children: List[Optional[Ring]],
                hot_pixels: List[Point],
-               current_hot_pixels_index: int,
+               current_hot_pixel_index: int,
                rings: List[Ring],
                index: int) -> None:
-    result = RingManager(children, hot_pixels, current_hot_pixels_index,
+    result = RingManager(children, hot_pixels, current_hot_pixel_index,
                          rings, index)
 
     assert result.children == children
     assert result.hot_pixels == hot_pixels
-    assert result.current_hot_pixels_index == min(current_hot_pixels_index,
-                                                  len(hot_pixels))
+    assert result.current_hot_pixel_index == min(current_hot_pixel_index,
+                                                 len(hot_pixels))
     assert result.rings == rings
     assert result.index == index
