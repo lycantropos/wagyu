6eea7ae5a9c3b41aad6dd0015aed90b4cf667bab
Azat Ibrakov <azatibrakov@gmail.com>
2020-07-22 04:27:55 +0700

    Add test for bound & ported 'RingManager.add_first_point' methods

---
+++ tests/integration_tests/ring_managers_tests/test_add_first_point.py
@@ -0,0 +1,35 @@
+from typing import Tuple
+
+from hypothesis import given
+
+from tests.utils import (BoundPortedBoundsListsPair,
+                         BoundPortedPointsPair,
+                         BoundPortedRingManagersPair,
+                         are_bound_ported_bounds_equal,
+                         are_bound_ported_bounds_lists_equal,
+                         are_bound_ported_ring_managers_equal)
+from . import strategies
+
+
+@given(strategies.ring_managers_pairs,
+       strategies.non_empty_bounds_lists_pairs_with_indices,
+       strategies.points_pairs)
+def test_basic(pair: BoundPortedRingManagersPair,
+               active_bounds_pair_index: Tuple[BoundPortedBoundsListsPair,
+                                               int],
+               points_pair: BoundPortedPointsPair) -> None:
+    bound, ported = pair
+    ((bound_active_bounds, ported_active_bounds),
+     index) = active_bounds_pair_index
+    bound_point, ported_point = points_pair
+    ported_bound = ported_active_bounds[index]
+
+    bound_active_bounds = bound.add_first_point(index, bound_active_bounds,
+                                                bound_point)
+    ported.add_first_point(ported_bound, ported_active_bounds, ported_point)
+
+    assert are_bound_ported_bounds_equal(bound_active_bounds[index],
+                                         ported_bound)
+    assert are_bound_ported_bounds_lists_equal(bound_active_bounds,
+                                               ported_active_bounds)
+    assert are_bound_ported_ring_managers_equal(bound, ported)
