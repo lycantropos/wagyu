3aa62ec107bac128761b18ee29a8f37433290e27
Azat Ibrakov <azatibrakov@gmail.com>
2020-07-22 03:43:39 +0700

    Add test for bound & ported 'Ring.recalculate_stats' methods

---
+++ tests/integration_tests/rings_tests/test_recalculate_stats.py
@@ -0,0 +1,21 @@
+import math
+
+from hypothesis import given
+
+from tests.utils import (BoundPortedRingsPair,
+                         are_bound_ported_boxes_equal)
+from . import strategies
+
+
+@given(strategies.rings_pairs)
+def test_basic(rings_pair: BoundPortedRingsPair) -> None:
+    bound, ported = rings_pair
+
+    bound.recalculate_stats()
+    ported.recalculate_stats()
+
+    assert (math.isnan(bound.area) and math.isnan(ported.area)
+            or bound.area == ported.area)
+    assert bound.size == ported.size
+    assert are_bound_ported_boxes_equal(bound.box, ported.box)
+    assert bound.is_hole == ported.is_hole
