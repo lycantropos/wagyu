3f929eee68b6885102eb3324005e38cbdc06168d
Azat Ibrakov <azatibrakov@gmail.com>
2020-07-24 02:37:12 +0700

    Add test for bound & ported 'Bound.is_intermediate' methods

---
+++ tests/integration_tests/bounds_tests/test_is_intermediate.py
@@ -0,0 +1,13 @@
+from hypothesis import given
+
+from tests.utils import (BoundPortedBoundsPair,
+                         equivalence)
+from wagyu.hints import Coordinate
+from . import strategies
+
+
+@given(strategies.initialized_bounds_pairs, strategies.coordinates)
+def test_basic(pair: BoundPortedBoundsPair, y: Coordinate) -> None:
+    bound, ported = pair
+
+    assert equivalence(bound.is_intermediate(y), ported.is_intermediate(y))
