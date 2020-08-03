0ce1b1cb3cc836959cf432a5d912912a700b1920
Azat Ibrakov <azatibrakov@gmail.com>
2020-08-03 16:54:00 +0700

    Add test for bound & ported 'Box.inside_of' methods

---
+++ tests/integration_tests/boxes_tests/test_inside_of.py
@@ -0,0 +1,15 @@
+from hypothesis import given
+
+from tests.utils import (BoundPortedBoxesPair,
+                         equivalence)
+from . import strategies
+
+
+@given(strategies.boxes_pairs, strategies.boxes_pairs)
+def test_basic(first_pair: BoundPortedBoxesPair,
+               second_pair: BoundPortedBoxesPair) -> None:
+    first_bound, first_ported = first_pair
+    second_bound, second_ported = second_pair
+
+    assert equivalence(first_bound.inside_of(second_bound),
+                       first_ported.inside_of(second_ported))
