b29d10a0d6db649678525f6b7c0f3e6fe12fac8d
Azat Ibrakov <azatibrakov@gmail.com>
2020-08-03 19:34:20 +0700

    Add test for bound & ported 'RingManager.sorted_rings' properties

---
+++ tests/integration_tests/ring_managers_tests/test_sorted_rings.py
@@ -0,0 +1,13 @@
+from hypothesis import given
+
+from tests.utils import (BoundPortedRingManagersPair,
+                         are_bound_ported_rings_lists_equal)
+from . import strategies
+
+
+@given(strategies.ring_managers_pairs)
+def test_basic(pair: BoundPortedRingManagersPair) -> None:
+    bound, ported = pair
+
+    assert are_bound_ported_rings_lists_equal(bound.sorted_rings,
+                                              ported.sorted_rings)
