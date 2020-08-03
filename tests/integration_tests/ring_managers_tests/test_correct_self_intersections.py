557963cb76ed5f5086b8cfa39d4c87ec46a1dbfc
Azat Ibrakov <azatibrakov@gmail.com>
2020-08-03 19:49:05 +0700

    Add test for bound & ported 'RingManager.correct_self_intersections' methods

---
+++ tests/integration_tests/ring_managers_tests/test_correct_self_intersections.py
@@ -0,0 +1,17 @@
+from hypothesis import given
+
+from tests.utils import (BoundPortedRingManagersPair,
+                         are_bound_ported_ring_managers_equal,
+                         equivalence)
+from . import strategies
+
+
+@given(strategies.ring_managers_pairs, strategies.booleans)
+def test_basic(pair: BoundPortedRingManagersPair, correct_tree: bool) -> None:
+    bound, ported = pair
+
+    bound_result = bound.correct_self_intersections(correct_tree)
+    ported_result = ported.correct_self_intersections(correct_tree)
+
+    assert equivalence(bound_result, ported_result)
+    assert are_bound_ported_ring_managers_equal(bound, ported)
